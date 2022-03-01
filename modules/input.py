028c5c075aa0adc26b0fefc9d5e51415c2cb47bf
ARSENIIQ <98899794+ARSENIIQ@users.noreply.github.com>
2022-03-01 15:14:00 +0300

    Update input.py
    pep8

--- modules/input.py
+++ modules/input.py
@@ -4,7 +4,8 @@ class Input:
 
     input_state = "main"
 
-    record_keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    record_keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                   "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
                    "UP", "DOWN", "LEFT", "RIGHT", 
                    "SPACE", "CTRL", "SHIFT", "ALT", "RETURN", "ESCAPE", "TAB", "BACKSPACE"]
